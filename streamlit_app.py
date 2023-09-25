bf421e0395c28321f951f225a12d7a02070c4294
starbattlecar <145995754+starbattlecar@users.noreply.github.com>
2023-09-25 15:50:57 +0200

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,32 @@
 import streamlit as st
+import numpy as np
+import altair as alt
+import pandas as pd
 
-st.header('st.button')
 
-if st.button('Say Hello'):
-    st.write('Howzit')
-else:
-    st.write('Goodbye')
+st.header('st.write')
+
+# example 1
+st.write('Hello world!* :sunglasses:')
+
+# example 2
+st.write(1234)
+
+# example 3
+df = pd.DataFrame(
+    {'first_column': [1, 2, 3, 4],
+     'second_column': [10, 20, 30, 40]
+     }
+)
+st.write(df)
+
+# example 4
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+# example 5
+df2 = pd.DataFrame(
+    np.random.randn(200, 3),
+    columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
